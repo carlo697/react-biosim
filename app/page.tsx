a3dc88d12abfb835dd5942b79cf0d40a1a96c90b
carlo697 <carlos.07888@gmail.com>
2023-03-21 00:44:44 -0400

    Add empty index page to test tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Page</main>;
+  return (
+    <main className="flex items-center justify-center min-h-screen text-white bg-slate-600">
+      <h1>Page</h1>
+    </main>
+  );
 }
