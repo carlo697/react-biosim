1e4723a7cea9c3a9f745ca06f31a865f0bda1e41
carlo697 <carlos.07888@gmail.com>
2023-03-26 23:02:53 -0400

    Add flex-wrap to the 'TabList' component

--- components/global/tabs/TabList.tsx
+++ components/global/tabs/TabList.tsx
@@ -1,5 +1,5 @@
 interface Props extends React.PropsWithChildren {}
 
 export default function TabList({ children }: Props) {
-  return <div className={"-m-0.5 flex"}>{children}</div>;
+  return <div className={"-m-0.5 flex flex-wrap"}>{children}</div>;
 }
