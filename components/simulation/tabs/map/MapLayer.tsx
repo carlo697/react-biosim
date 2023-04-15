5638d21efe934efd1d321bfb886a0606272c302c
carlo697 <carlos.07888@gmail.com>
2023-04-15 17:08:52 -0400

    Add buttons to move layers in the map painter

--- components/simulation/tabs/map/MapLayer.tsx
+++ components/simulation/tabs/map/MapLayer.tsx
@@ -7,8 +7,10 @@ import { useWindowSize } from "react-use";
 import {
   painterWorldSizeAtom,
   painterSelectedObjectIndexAtom,
+  painterObjectsAtom,
 } from "../../store/mapPainterAtoms";
 import classNames from "classnames";
+import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 
 interface Props {
   index: number;
@@ -19,12 +21,15 @@ export function getName(obj: Object) {
   return Object.getPrototypeOf(obj).constructor.name;
 }
 
+const upDownButtonClassnames = "h-1/2 px-0.5 hover:brightness-75 bg-grey-mid";
+
 export default function MapLayer({ index, obj }: Props) {
   const worldSize = useAtomValue(painterWorldSizeAtom);
   const canvas = useRef<HTMLCanvasElement>(null);
   const [selectedObjectIndex, setSelectedObjectIndex] = useAtom(
     painterSelectedObjectIndexAtom
   );
+  const [objects, setObjects] = useAtom(painterObjectsAtom);
 
   const draw = useCallback(() => {
     if (canvas.current) {
@@ -48,17 +53,55 @@ export default function MapLayer({ index, obj }: Props) {
     setSelectedObjectIndex(index);
   };
 
+  const handleMoveLayer = useCallback(
+    (movement: 1 | -1) => {
+      const newObjects = [...objects];
+      const object = newObjects[index];
+      newObjects.splice(index, 1);
+      newObjects.splice(index + movement, 0, object);
+
+      // Also move the selected index
+      if (selectedObjectIndex === index) {
+        setSelectedObjectIndex(selectedObjectIndex + movement);
+      }
+
+      setObjects(newObjects);
+    },
+    [index, objects, selectedObjectIndex, setObjects, setSelectedObjectIndex]
+  );
+
   return (
-    <button
+    <div
       className={classNames(
-        "flex w-full items-center gap-2 border-2 bg-grey-mid",
+        "relative overflow-hidden border-2 bg-grey-mid",
         selectedObjectIndex === index ? "border-white" : "border-transparent"
       )}
-      onClick={handleClick}
     >
-      <canvas className="aspect-square h-10 bg-white" ref={canvas}></canvas>
+      <button className="flex w-full items-center gap-2" onClick={handleClick}>
+        <canvas className="aspect-square h-10 bg-white" ref={canvas}></canvas>
 
-      <h4>{getName(obj)}</h4>
-    </button>
+        <h4>{getName(obj)}</h4>
+      </button>
+
+      <div className="absolute right-0 top-0 flex h-full flex-col bg-grey-mid">
+        {index > 0 && (
+          <button
+            className={upDownButtonClassnames}
+            onClick={() => handleMoveLayer(-1)}
+          >
+            <AiOutlineCaretUp />
+          </button>
+        )}
+
+        {index < objects.length - 1 && (
+          <button
+            className={upDownButtonClassnames}
+            onClick={() => handleMoveLayer(1)}
+          >
+            <AiOutlineCaretDown />
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
