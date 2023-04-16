437725d90a4d2373d3b98701cf446336d1620369
carlo697 <carlos.07888@gmail.com>
2023-04-16 19:12:00 -0400

    Add select input for adding objects to the map painter

--- components/simulation/tabs/map/MapPainterFooter.tsx
+++ components/simulation/tabs/map/MapPainterFooter.tsx
@@ -1,9 +1,81 @@
+import SelectInput from "@/components/global/inputs/SelectInput";
 import MapObjectProperties from "./MapObjectProperties";
+import WorldObject from "@/simulation/world/WorldObject";
+import RectangleObject from "@/simulation/world/objects/RectangleObject";
+import { useAtom, useSetAtom } from "jotai";
+import {
+  painterObjectsAtom,
+  painterSelectedObjectIndexAtom,
+} from "../../store/mapPainterAtoms";
+import EllipseObject from "@/simulation/world/objects/EllipseObject";
+import RectangleReproductionArea from "@/simulation/world/areas/reproduction/RectangleReproductionArea";
+import EllipseReproductionArea from "@/simulation/world/areas/reproduction/EllipseReproductionArea";
+
+const defaultX = 0;
+const defaultY = 0;
+const defaultWidth = 0.2;
+const defaultHeight = 0.2;
+
+type MapPainterAddObjectPreset = {
+  name: string;
+  create: () => WorldObject;
+};
+
+const addPresets: Record<string, MapPainterAddObjectPreset> = {
+  rectangle: {
+    name: "Rectangle",
+    create: () =>
+      new RectangleObject(defaultX, defaultY, defaultWidth, defaultHeight),
+  },
+  ellipse: {
+    name: "Ellipse",
+    create: () =>
+      new EllipseObject(defaultX, defaultY, defaultWidth, defaultHeight),
+  },
+  rectangleReproduction: {
+    name: "Rectangle Reproduction",
+    create: () =>
+      new RectangleReproductionArea(
+        defaultX,
+        defaultY,
+        defaultWidth,
+        defaultHeight
+      ),
+  },
+  ellipseReproduction: {
+    name: "Ellipse Reproduction",
+    create: () =>
+      new EllipseReproductionArea(
+        defaultX,
+        defaultY,
+        defaultWidth,
+        defaultHeight
+      ),
+  },
+};
 
 export default function MapPainterFooter() {
+  const [objects, setObjects] = useAtom(painterObjectsAtom);
+  const setSelectedIndex = useSetAtom(painterSelectedObjectIndexAtom);
+
+  const handleSelect = (value: string) => {
+    setObjects([...objects, addPresets[value].create()]);
+    setSelectedIndex(objects.length);
+  };
+
   return (
     <div className="grid grid-cols-2 gap-5">
-      <div></div>
+      <div>
+        <SelectInput label="Add object" onChange={handleSelect}>
+          <option value="placeholder">Select an object...</option>
+          {Object.entries(addPresets).map(([key, { name }]) => (
+            <option value={key} key={key}>
+              {name}
+            </option>
+          ))}
+        </SelectInput>
+      </div>
+      
       <div>
         <MapObjectProperties />
       </div>
