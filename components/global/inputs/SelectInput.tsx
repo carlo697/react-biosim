a49c7391ef2ed1cb4f5078712d82bc10ee451582
carlo697 <carlos.07888@gmail.com>
2023-04-16 19:07:05 -0400

    Add 'onChange' prop to the 'SelectInput' component

--- components/global/inputs/SelectInput.tsx
+++ components/global/inputs/SelectInput.tsx
@@ -1,21 +1,29 @@
-import { ReactNode } from "react";
-import { PrimitiveAtom, useAtom } from "jotai";
-import { EnumType } from "typescript";
+import { ReactNode, useState } from "react";
+import { PrimitiveAtom, atom as newAtom, useAtom } from "jotai";
 
 interface Props extends React.PropsWithChildren {
-  atom: PrimitiveAtom<any>;
+  onChange?: (value: any) => void;
+  atom?: PrimitiveAtom<any>;
   label?: ReactNode;
 }
 
-export default function SelectInput({ atom, label, children }: Props) {
-  const [currentValue, setCurrentValue] = useAtom(atom);
+export default function SelectInput({
+  onChange,
+  atom,
+  label,
+  children,
+}: Props) {
+  const [defaultAtom] = useState(() => newAtom<any>(0));
+  const [currentValue, setCurrentValue] = useAtom(atom ?? defaultAtom);
 
   return (
     <div className="flex flex-col">
       {label && <label className="grow">{label}</label>}
       <select
         value={currentValue.toString()}
-        onChange={(e) => setCurrentValue(e.target.value)}
+        onChange={(e) =>
+          onChange ? onChange(e.target.value) : setCurrentValue(e.target.value)
+        }
         className="min-w-0 bg-grey-mid p-1"
       >
         {children}
