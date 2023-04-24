3876b1a8a8249cc1765ee4a9d0d50220bb69e367
carlo697 <carlos.07888@gmail.com>
2023-04-23 22:44:01 -0400

    feat: update 'Button' component to support icons

--- components/global/Button.tsx
+++ components/global/Button.tsx
@@ -1,25 +1,34 @@
 import classNames from "classnames";
+import { ReactNode } from "react";
 
 interface Props
   extends React.PropsWithChildren,
     React.ComponentPropsWithoutRef<"button"> {
   variant?: "dark" | "danger" | "grey";
+  icon?: ReactNode;
 }
 
 export default function Button({
   children,
+  className,
   variant = "danger",
+  icon,
   ...rest
 }: Props) {
-  const className = classNames(
-    "py-1 px-3 lg:py-2 lg:px-4 text-sm lg:text-base hover:brightness-90 rounded-md",
+  const finalClassName = classNames(
+    "flex items-center gap-2 text-sm lg:text-base hover:brightness-90 rounded-md",
+    !icon && "py-1 px-3 lg:py-2 lg:px-4",
+    icon && "p-1 lg:p-2",
+    icon && !children && "aspect-square",
     variant === "dark" && "bg-grey-dark text-white",
     variant === "danger" && "bg-red text-whitef",
-    variant === "grey" && "bg-grey-mid text-white"
+    variant === "grey" && "bg-grey-mid text-white",
+    className
   );
 
   return (
-    <button className={className} {...rest}>
+    <button className={finalClassName} {...rest}>
+      {icon && <span>{icon}</span>}
       {children}
     </button>
   );
