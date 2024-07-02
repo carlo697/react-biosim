50cd687fe0e13d5ccf9fce92df8e9a233e21ca4c
carlo697 <carlos.07888@gmail.com>
2024-07-02 17:12:46 -0400

    feat: add meta tag "google-site-verification"

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata = {
   title: "Evolution Simulation",
   description:
     "Evolution simulation in the browser inspired by Biosim from David R. Miller",
+  verification: {
+    google: process.env.GOOGLE_SITE_VERIFICATION,
+  },
 };
 
 export default function RootLayout({
